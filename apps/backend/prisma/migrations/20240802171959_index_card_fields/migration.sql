6728eebed015ef9f46e389c304dfe27346b3f2b5
Joshua Sebastian Kim <joshuakim150@gmail.com>
2024-08-02 14:20:51 -0300

    Add index for card fields

---
+++ apps/backend/prisma/migrations/20240802171959_index_card_fields/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "Card_type_idx" ON "Card"("type");
+
+-- CreateIndex
+CREATE INDEX "Card_resistance_idx" ON "Card"("resistance");
+
+-- CreateIndex
+CREATE INDEX "Card_weakness_idx" ON "Card"("weakness");
+
+-- CreateIndex
+CREATE INDEX "Card_rarity_idx" ON "Card"("rarity");
