7ae50a4d00254177183610ba1208bb7e1a5c19c4
Joshua Sebastian Kim <joshuakim150@gmail.com>
2024-08-06 21:41:19 -0300

    Add link to title

--- apps/frontend/app/_components/Appbar.tsx
+++ apps/frontend/app/_components/Appbar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import SignoutButton from "./SignoutButton";
+import Link from "next/link";
 
 const Appbar = () => {
   return (
     <header className="flex justify-end gap-4 p-4 shadow">
-      <h1 className="text-2xl font-bold leading-7 text-gray-900 mr-auto">
-        Pokémon App
-      </h1>
+      <Link href="/card-list">
+        <h1 className="text-2xl font-bold leading-7 text-gray-900 mr-auto">
+          Pokémon App
+        </h1>
+      </Link>
+
       <SignoutButton />
     </header>
   );
